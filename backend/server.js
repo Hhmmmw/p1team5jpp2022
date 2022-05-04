40e34e0dc36af2174e7805d3601a9bd084b36607
HHMMMW <computetional.h@gmail.com>
2022-05-04 21:43:48 +0200

    fix

--- backend/server.js
+++ backend/server.js
@@ -165,7 +165,7 @@ app.get('/api/products', async (req, res) => {
       const col = db.collection('products');
       let resutls = [];
       // col.find(kw!=='' ? { $text: { $search: kw } } : {}).limit(500).forEach(function (doc) {
-      col.find(kw !== '' ? [
+      col.find(kw !== '' ? 
         {
           '$search': {
             'index': 'default',
@@ -177,7 +177,7 @@ app.get('/api/products', async (req, res) => {
             }
           }
         }
-      ]: {}).limit(500).forEach(function (doc) {
+      : {}).limit(500).forEach(function (doc) {
         if (doc) {
           // console.log(doc)
           resutls.push(doc)
